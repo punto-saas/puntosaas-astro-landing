5bea4e80100ee55a42586f0261326991c7937230
jaimersb <jaimersb@gmail.com>
2025-04-14 18:20:01 -0500

    Added React Framwork

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [mdx(), sitemap(), icon(), react()],
   vite: {
     plugins: [tailwindcss()],
   },
